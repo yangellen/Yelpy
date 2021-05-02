220a7e10c169568958ae07043ad65bdd680ff1d7
Ellen Yang <yange@oregonstate.edu>
2021-05-02 14:21:41 -0700

    Add rating

--- Yelpy/Controllers/RestaurantsViewController.swift
+++ Yelpy/Controllers/RestaurantsViewController.swift
@@ -64,7 +64,42 @@ class RestaurantsViewController: UIViewController, UITableViewDataSource, UITabl
       if let rating = restaurant["rating"] as? Double {
          print(rating)
 
-        //
+         switch rating {
+         case 0:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_0")
+
+         case 1:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_1")
+
+         case 1.5:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_1_half")
+
+         case 2:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_2")
+
+         case 2.5:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_2_half")
+
+         case 3:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_3")
+
+         case 3.5:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_3_half")
+
+         case 4:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_4")
+
+         case 4.5:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_4_half")
+
+         case 5:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_5")
+
+         default:
+            cell.ratingImage.image = #imageLiteral(resourceName: "small_0")
+         }
+         
+
       }
 
 
